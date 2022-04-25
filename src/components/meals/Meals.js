d7e5059fbd1cf864ef358ae1962ac5704f8c3a73
TimZZZ19 <53802591+TimZZZ19@users.noreply.github.com>
2022-04-25 12:01:01 -0500

    finished menu item, next: shopping cart

---
+++ src/components/meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+import React from "react";
+
+export default function Meals() {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+}
