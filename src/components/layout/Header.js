b7a33343d21554bf02d92b1f45d7bea0f83cbba3
TimZZZ19 <53802591+TimZZZ19@users.noreply.github.com>
2022-04-24 22:20:50 -0500

    added header and header cart button and finished the stylings for those items

---
+++ src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import { React } from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+
+import HeaderCartButton from "./HeaderCartButton";
+
+export default function Header() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table of declious food" />
+      </div>
+    </>
+  );
+}
